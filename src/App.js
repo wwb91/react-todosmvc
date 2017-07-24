4b2bc49d0dd5d3bec0df4d3130cd4b5f8994fd06
cnwang <cnwangjune@gmail.com>
2017-07-24 16:07:46 +0800

    mark completed status

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+import 'normalize.css'
+import 'reset.css'
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: '',
+      todoList: [
+      ]
+    }
+  }
   render() {
+
+    let todos = this.state.todoList.map((item, index) => {
+      return (
+        <li key={index} >
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)} />
+        </li>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo}
+            onchange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ol>
+          {todos}
+        </ol>
       </div>
-    );
+    )
+  }
+
+
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+  toggle(e, todo) {
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state);
+  }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
   }
 }
 
 export default App;
+
+let id = 0
+
+function idMaker() {
+  id += 1
+  return id
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import Welcome from './Welcome';
 
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
-);
+)
